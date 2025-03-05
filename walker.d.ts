43ff89eb3f22e3f073ea19212258745cd79b7de9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2025-03-05 20:19:47 +0900

    fix: incorrect type in walker.d.ts (#2)
    * fix: incorrect type in walker.d.ts
    
    * Update walker.d.ts

--- walker.d.ts
+++ walker.d.ts
@@ -1,7 +1,7 @@
 export default async function htmlStreamWalker(
   streamReader: ReadableStreamDefaultReader<Uint8Array>,
-): {
+): Promise<{
   rootNode: Node | null;
-  firstChild: (node: Node) => Node | null;
-  nextSibling: (node: Node) => Node | null;
-}
+  firstChild: (node: Node) => Promise<Node | null>;
+  nextSibling: (node: Node) => Promise<Node | null>;
+}>
